f3b4d5bb980b7c63ee782e1f4019b6131c806a73
nguyentrongdoanh <nguyentrongdoanh202@gmail.com>
2024-09-23 00:28:15 +0700

    first commit

---
+++ hello-spring-boot/src/test/java/com/ndtdoanh/hello_spring_boot/HelloSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndtdoanh.hello_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
