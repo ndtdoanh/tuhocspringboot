4435f864ae10ef4e6bb42a6a1a62d59640b9d67c
nguyentrongdoanh <nguyentrongdoanh202@gmail.com>
2024-09-23 00:41:02 +0700

    hello world

---
+++ hello-spring-boot/src/main/java/com/ndtdoanh/hello_spring_boot/controller/HelloSpringController.java
@@ -0,0 +1,12 @@
+package com.ndtdoanh.hello_spring_boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringController {
+   @GetMapping("/hello")
+    String sayHello() {
+       return "Hello spring boot 3, nice to meet you";
+   }
+}
