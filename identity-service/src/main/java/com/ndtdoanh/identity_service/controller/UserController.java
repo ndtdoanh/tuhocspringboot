55d0d86140a006b8a5339e4008426ee25573199b
nguyentrongdoanh <nguyentrongdoanh202@gmail.com>
2024-09-23 02:15:41 +0700

    controller

--- identity-service/src/main/java/com/ndtdoanh/identity_service/controller/UserController.java
+++ identity-service/src/main/java/com/ndtdoanh/identity_service/controller/UserController.java
@@ -1,4 +1,20 @@
 package com.ndtdoanh.identity_service.controller;
 
+import com.ndtdoanh.identity_service.dto.request.UserCreationRequest;
+import com.ndtdoanh.identity_service.entity.User;
+import com.ndtdoanh.identity_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users")
+    User createUser(@RequestBody UserCreationRequest request) {
+        return userService.createUser(request);
+    }
 }
