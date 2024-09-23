abe041d9da457aa2ce705d3e47a95be8138a5e13
nguyentrongdoanh <nguyentrongdoanh202@gmail.com>
2024-09-23 21:59:07 +0700

    validation

--- identity-service/src/main/java/com/ndtdoanh/identity_service/exception/GlobalExceptionHanler.java
+++ identity-service/src/main/java/com/ndtdoanh/identity_service/exception/GlobalExceptionHanler.java
@@ -1,6 +1,7 @@
 package com.ndtdoanh.identity_service.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,6 +11,11 @@ public class GlobalExceptionHanler {
     @ExceptionHandler(value = RuntimeException.class)
     ResponseEntity<String> handlingRuntimeException(RuntimeException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<String> handlingValidation(MethodArgumentNotValidException exception){
+        return ResponseEntity.badRequest().body(exception.getFieldError().getDefaultMessage());
     }
 
 }
