f19a1133474f852b15e8f22b83bc89808ade0d82
nguyentrongdoanh <nguyentrongdoanh202@gmail.com>
2024-09-23 19:01:16 +0700

    api crud

--- identity-service/src/main/java/com/ndtdoanh/identity_service/controller/UserController.java
+++ identity-service/src/main/java/com/ndtdoanh/identity_service/controller/UserController.java
@@ -1,20 +1,43 @@
 package com.ndtdoanh.identity_service.controller;
 
 import com.ndtdoanh.identity_service.dto.request.UserCreationRequest;
+import com.ndtdoanh.identity_service.dto.request.UserUpdateRequest;
 import com.ndtdoanh.identity_service.entity.User;
 import com.ndtdoanh.identity_service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping
     User createUser(@RequestBody UserCreationRequest request) {
         return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId) {
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable String userId) {
+        userService.deleteUser(userId);
+        return "User has been deleted";
     }
 }
--- identity-service/src/main/java/com/ndtdoanh/identity_service/service/UserService.java
+++ identity-service/src/main/java/com/ndtdoanh/identity_service/service/UserService.java
@@ -1,10 +1,13 @@
 package com.ndtdoanh.identity_service.service;
 
 import com.ndtdoanh.identity_service.dto.request.UserCreationRequest;
+import com.ndtdoanh.identity_service.dto.request.UserUpdateRequest;
 import com.ndtdoanh.identity_service.entity.User;
 import com.ndtdoanh.identity_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -21,5 +24,28 @@ public class UserService {
         user.setDob(request.getDob());
 
         return userRepository.save(user);
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request){
+        User user = getUser(userId);
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
