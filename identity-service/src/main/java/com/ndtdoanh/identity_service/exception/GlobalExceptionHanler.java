ac281bd66d7817f5e208a86082b81eff80634258
nguyentrongdoanh <nguyentrongdoanh202@gmail.com>
2024-09-23 20:49:12 +0700

    Exception

---
+++ identity-service/src/main/java/com/ndtdoanh/identity_service/exception/GlobalExceptionHanler.java
@@ -0,0 +1,15 @@
+package com.ndtdoanh.identity_service.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHanler {
+
+    @ExceptionHandler(value = RuntimeException.class)
+    ResponseEntity<String> handlingRuntimeException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+}
